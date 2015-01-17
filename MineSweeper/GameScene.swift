7d1e452558a7a2fa3c6cc750d48df8e8a0264a0e
younippon <hikonyan.mh36@gmail.com>
2015-01-17 21:25:14 +0900

    test image added and make game scene

--- MineSweeper/GameScene.swift
+++ MineSweeper/GameScene.swift
@@ -10,100 +10,225 @@ import SpriteKit
 
 class GameScene: SKScene {
 
+    enum Phase{
+        case GetReady, Game, GameOver, GameClear
+    }
+    
+    //colors
+    let bgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+    let squareColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    let squareBorderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    let mineBrendColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+    let flagBrendColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+    
+    //numbers
     let rows = 5
     let cols = 5
-    let mineNum = 5
+    let mineNum = 1
     
-    let myLabel = SKLabelNode(fontNamed:"HelveticaNeue")
+    //sizes and margins
+    let fieldSize = 320.0
+    var squareFrameSize:CGFloat = 86.0
+    let marginButtom:CGFloat = 120.0
     
-    let areaEven = UIColor(red: 207.0 / 255.0, green: 216.0 / 255.0, blue: 220.0 / 255.0, alpha: 1)
-    let areaOdd = UIColor(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0, alpha: 1)
-    var playScene = false
-    var mineNumArray: Array<Int> = []
+    //parameter
+    var phase = Phase.GetReady
+    var score = 0
+    var openCount = 0
+    var timer : NSTimeInterval = 0
+    
+    //square type set
+    var squareTypeArray:Array<NSInteger> = []
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        myLabel.text = "MINE SWEEPER"
-        myLabel.fontSize = 36;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMaxY(self.frame) - 90.0);
-        myLabel.name = "-1"
-        self.addChild(myLabel)
-        mineNumArray = setMineNum()
-        addAreaBlocks()
+        self.backgroundColor = bgColor
+        getReady()
     }
     
     override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
-        for touch: AnyObject in touches {
-            
-            if (!playScene){
-                playScene = !playScene
-                myLabel.text = "START!"
-                return
-            }
-            
-            let location = touch.locationInNode(self)
-            let node:SKNode = nodeAtPoint(location)
-            println(node.name)
-            let touchNum = node.name?.toInt()
-            if (find(mineNumArray, touchNum!) != nil){
-                myLabel.text = "GAMEOVER"
-                playScene = !playScene
-                println("don!")
-            }
-            if (node.alpha > 0.5) {
-                node.alpha = 0.5
-            }
+        switch phase {
+        case .GetReady:
+            start()
+        case .Game:
+            gameAction(touches)
+        case .GameClear:
+            retry()
+        case .GameOver:
+            retry()
+        default:
+            println("touch Ended")
         }
+        
     }
    
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    func retry() {
+        score = 0
+        resetSquare()
+        
+        getReady()
     }
     
-    func addAreaBlocks(){
-        let blockSize: CGFloat = 86.25//self.frame.size.width / CGFloat(cols * 3)
+    func resetSquare() {
         
-        var y: CGFloat = 240.0
-        for i in 0...rows - 1{
-            var x: CGFloat = blockSize / 2.0 - blockSize * CGFloat(cols) / 2.0 + self.frame.size.width / 2.0
-            for j in 0...cols - 1{
-                var block = newAreaBlock(CGFloat(blockSize), blockColorId: Int(i+j))
-                block.position = CGPointMake(x, y)
-                block.name = String(cols * i + j)
-                println(block.name)
-                x += blockSize
+    }
+    
+    func getReady() {
+        let minePosition = setMinePosition()
+        addSquares(minePosition)
+        
+        
+        phase = Phase.GetReady
+
+    }
+    
+    func start() {
+        phase = Phase.Game
+    }
+    
+    func addSquares(minePosition: Array<NSInteger>) {
+        let squareSize = setSquareSize()
+        
+        for i in 0...rows - 1 {
+            for j in 0...cols - 1 {
+                
+                let n = setSquareNum(i, col: j)
+                let squareType = setSquareType(minePosition, xPos:j, yPos:i)
+                
+                var square = addSquare(squareSize, type:squareType)
+                square.name = NSString(format: "%d", n)
+                square.position = setSquarePosition(square, size:squareSize, xPos:j, yPos:i)
+                squareTypeArray.append(squareType)
+                //addChild(square)
             }
-            y += blockSize
+        }
+        
+    }
+    
+    func setSquareNum(row:NSInteger, col:NSInteger) -> NSInteger {
+        return cols * row + col
+    }
+    
+    func setSquareSize() -> CGFloat {
+        squareFrameSize = 430.0 / CGFloat(cols)
+        let squareSize = squareFrameSize//fieldSize / Double(rows)
+        return CGFloat(squareSize)
+    }
+    
+    func setSquareType (position:Array<NSInteger>, xPos:NSInteger, yPos:NSInteger) -> NSInteger {
+        var neighborMineNum = 0
+        
+        for i in 0...position.count - 1 {
+            let mineX:NSInteger = position[i] % cols
+            let mineY:NSInteger = position[i] / cols
+            
+            if(setSquareNum(yPos, col: xPos) == position[i]){
+                return 9
+            }else if(abs(mineX - xPos) <= 1 && abs(mineY - yPos) <= 1){
+                neighborMineNum += 1
+            }
+        }
+        return neighborMineNum
+    }
+    
+    func addSquare(size:CGFloat, type: NSInteger) -> SKSpriteNode {
+        
+        let imageName = setSquareImageName(type)
+        
+        var square = SKSpriteNode(imageNamed: imageName)
+        
+        square.xScale = squareFrameSize / (CGFloat(rows) * 2.0)
+        square.yScale = squareFrameSize / (CGFloat(rows) * 2.0)
+        //square.name = NSString(format: "%d", type)
+        square.color = mineBrendColor
+        square.colorBlendFactor = CGFloat(type) / 10.0
+        
+        addChild(square)
+        
+        return square
+        
+    }
+    
+    func setSquarePosition(square:SKSpriteNode, size:CGFloat, xPos:NSInteger, yPos:NSInteger) -> CGPoint {
+        
+        let x:CGFloat = size * (CGFloat(xPos) + 1.0) + self.frame.size.width / 4.0
+        let y:CGFloat = self.frame.size.height / 2.0 - CGFloat(yPos) * size + marginButtom
+        let point = CGPoint(x: x, y: y)
+        println(xPos, yPos, x, y)
+        
+        return point
+    }
+    
+    func setSquareImageName(type:NSInteger) -> NSString {
+        switch type {
+        case 0:
+            return "mine.png"
+        case 9:
+            return "mine.png"
+        default:
+            return NSString(format: "%d.png", type)
         }
     }
-
-    func setMineNum() -> Array<Int> {
-        
-        let areaNum:UInt32 = UInt32(cols * rows)
+    
+    func setMinePosition() -> Array<Int> {
+        let squareNum:UInt32 = UInt32(cols * rows)
         var randArray:Array<Int> = []
         
-        while randArray.count <= mineNum {
-            let randNum = Int(arc4random() % areaNum)
+        while randArray.count < mineNum {
+            let randNum = Int(arc4random() % squareNum)
             if (find(randArray, randNum) == nil) {
                 randArray.append(randNum)
             }
         }
         println(randArray)
+        
         return randArray
     }
     
-    func newAreaBlock(blockSize: CGFloat, blockColorId: Int) -> SKSpriteNode {
-        var blockColor: UIColor
-        if(blockColorId % 2 == 0){
-            blockColor = areaEven
-        }else{
-            blockColor = areaOdd
-        }
-        var block = SKSpriteNode(color: blockColor, size: CGSize(width: blockSize, height: blockSize))
-        addChild(block)
-        return block
+    //game action
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
     }
     
+    func gameAction (touches: NSSet) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            openSquare(location)
+        }
+    }
+    
+    func openSquare (location: CGPoint) {
+        let node:SKNode = nodeAtPoint(location)
+        let nodeName:String = node.name!
+        let type = squareTypeArray[nodeName.toInt()!]
+        
+        openSquareAction(type, node:node)
+        gameClearChecker()
+        
+    }
+    
+    func openSquareAction (type:NSInteger, node:SKNode) {
+        if(type == 0){
+            //todo:複数開く
+            node.alpha = 0.0
+            openCount += 1
+        }else if(type == 9){
+            phase = Phase.GameOver
+            println("gameover")
+        }else{
+            //todo:１つ開く
+            node.alpha = 0.0
+            openCount += 1
+        }
+    }
+    
+    func gameClearChecker (){
+        if(openCount >= rows * cols - mineNum) {
+            phase = Phase.GameClear
+            println("clear")
+        }
+    }
 }
